c7d35c0d556cd24e4277628fe0c469d681d1c688
凡向阳 <1346569137@qq.com>
2020-05-19 21:16:07 +0800

    v0.4.0

--- Classes/Core/FanRSA.m
+++ Classes/Core/FanRSA.m
@@ -296,7 +296,7 @@ static NSData *base64_decode(NSString *str){
                                &outlen
                                );
         if (status != 0) {
-            NSLog(@"SecKeyEncrypt fail. Error Code: %d", status);
+            NSLog(@"SecKeyEncrypt fail. Error Code: %ld", (long)status);
             ret = nil;
             break;
         }else{
@@ -377,7 +377,7 @@ static NSData *base64_decode(NSString *str){
                                &outlen
                                );
         if (status != 0) {
-            NSLog(@"SecKeyEncrypt fail. Error Code: %d", status);
+            NSLog(@"SecKeyEncrypt fail. Error Code: %ld", (long)status);
             ret = nil;
             break;
         }else{
