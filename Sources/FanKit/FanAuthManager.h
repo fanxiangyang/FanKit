0773301cda8810162df336e48287f8df05962b51
凡向阳 <fanxiangyang_heda@163.com>
2023-02-10 16:40:31 +0800

    [new]#保存相册
    1。只写权限
    2.保存相册

--- Sources/FanKit/FanAuthManager.h
+++ Sources/FanKit/FanAuthManager.h
@@ -17,12 +17,22 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 #pragma mark - 相册权限
-/// 请求相册权限
+/// 请求相册读写权限
 /// @param albumBlock （-2 用户点击不允许  -1本来就没有权限  0- 询问  1允许）
 +(void)requestAlbumAuthorizationBlock:(void(^)(NSInteger status))albumBlock;
-/// 当前相册权限（ -1本来就没有权限  0- 询问  1允许）
+/// 当前相册读写权限（ -1本来就没有权限  0- 询问  1允许）
 +(NSInteger)albumAuthorizationStatus;
-
+/// 请求相册读权限-（只有写iOS14+）
+/// @param albumBlock （-2 用户点击不允许  -1本来就没有权限  0- 询问  1允许）
++(void)requestAlbumAuthorizationOnlyWriteBlock:(void(^)(NSInteger status))albumBlock;
+/// 当前相册（只有写）权限iOS14+（ -1本来就没有权限  0- 询问  1允许）
++(NSInteger)albumAuthorizationStatusOnlyWrite;
+/// 保存到相册-图片UIImage、视频NSString
+/// - Parameters:
+///   - data: UIImage NSString NSURL
+///   - isImage: 是否是图片还是视频
+///   - completion: （isSuccess=成功、失败，identifier=相册标识符）
++(void)saveToAlbum:(id)data isImage:(BOOL)isImage completion:(void (^)(BOOL isSuccess,NSString *_Nullable identifier))completion;
 #pragma mark - 相机麦克风权限
 // ???: 为什么权限回调不执行
 // 请求权限与dispatch_group_wait有冲突，导致回调不执行，请使用嵌套权限处理
--- Sources/FanKit/FanAuthManager.m
+++ Sources/FanKit/FanAuthManager.m
@@ -9,6 +9,7 @@
 #import "FanAuthManager.h"
 #import <Photos/Photos.h>
 #import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
 
 
 @implementation FanAuthManager
@@ -24,7 +25,7 @@
 }
 
 #pragma mark - 相册权限
-/// 请求相册权限
+/// 请求相册读写权限
 /// @param albumBlock （-2 用户点击不允许  -1本来就没有权限  0- 询问  1允许）
 +(void)requestAlbumAuthorizationBlock:(void(^)(NSInteger status))albumBlock{
     PHAuthorizationStatus photoAuthorStatus=0;
@@ -81,7 +82,7 @@
         }
     }
 }
-/// 当前相册权限（ -1本来就没有权限  0- 询问  1允许）
+/// 当前相册读写权限（ -1本来就没有权限  0- 询问  1允许）
 +(NSInteger)albumAuthorizationStatus{
     PHAuthorizationStatus photoAuthorStatus=0;
     if (@available(iOS 14, *)) {
@@ -100,6 +101,135 @@
         //允许访问  PHAuthorizationStatusAuthorized  PHAuthorizationStatusLimited
         return 1;
     }
+}
+/// 请求相册读权限-（只有写iOS14+）
+/// @param albumBlock （-2 用户点击不允许  -1本来就没有权限  0- 询问  1允许）
++(void)requestAlbumAuthorizationOnlyWriteBlock:(void(^)(NSInteger status))albumBlock{
+    PHAuthorizationStatus photoAuthorStatus=0;
+    if (@available(iOS 14, *)) {
+        PHAccessLevel level = PHAccessLevelAddOnly;
+        photoAuthorStatus = [PHPhotoLibrary authorizationStatusForAccessLevel:level];
+    } else {
+        photoAuthorStatus = [PHPhotoLibrary authorizationStatus];
+    }
+    if (photoAuthorStatus==PHAuthorizationStatusDenied||photoAuthorStatus==PHAuthorizationStatusRestricted) {
+        //用户拒绝当前应用访问相册,我们需要提醒用户打开访问开关 ||  不允许访问
+        if (albumBlock) {
+            albumBlock(-1);
+        }
+    }else if (photoAuthorStatus==PHAuthorizationStatusNotDetermined){
+        //获取用户对是否允许访问相册的操作
+        if (@available(iOS 14, *)) {
+            [PHPhotoLibrary requestAuthorizationForAccessLevel:PHAccessLevelAddOnly handler:^(PHAuthorizationStatus status) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (status == PHAuthorizationStatusAuthorized||status== PHAuthorizationStatusLimited) {
+                        if (albumBlock) {
+                            albumBlock(1);
+                        }
+                    }else{
+                        if (albumBlock) {
+                            albumBlock(-2);
+                        }
+                    }
+                });
+            }];
+        } else {
+            [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (status==PHAuthorizationStatusAuthorized) {
+                        if (albumBlock) {
+                            albumBlock(1);
+                        }
+                    }else{
+                        if (albumBlock) {
+                            albumBlock(-2);
+                        }
+                    }
+                });
+            }];
+        }
+        if (albumBlock) {
+            albumBlock(0);
+        }
+        return;
+    }else{
+        //允许访问  PHAuthorizationStatusAuthorized  PHAuthorizationStatusLimited
+        if (albumBlock) {
+            albumBlock(1);
+        }
+    }
+}
+/// 当前相册（只有写）权限iOS14+（ -1本来就没有权限  0- 询问  1允许）
++(NSInteger)albumAuthorizationStatusOnlyWrite{
+    PHAuthorizationStatus photoAuthorStatus=0;
+    if (@available(iOS 14, *)) {
+        PHAccessLevel level = PHAccessLevelAddOnly;
+        photoAuthorStatus = [PHPhotoLibrary authorizationStatusForAccessLevel:level];
+    } else {
+        photoAuthorStatus = [PHPhotoLibrary authorizationStatus];
+    }
+    if (photoAuthorStatus==PHAuthorizationStatusDenied||photoAuthorStatus==PHAuthorizationStatusRestricted) {
+        //用户拒绝当前应用访问相册,我们需要提醒用户打开访问开关 ||  不允许访问
+        return -1;
+    }else if (photoAuthorStatus==PHAuthorizationStatusNotDetermined){
+        //获取用户对是否允许访问相册的操作
+        return 0;
+    }else{
+        //允许访问  PHAuthorizationStatusAuthorized  PHAuthorizationStatusLimited
+        return 1;
+    }
+}
+/// 保存到相册-图片UIImage、视频NSString
+/// - Parameters:
+///   - data: UIImage NSString NSURL
+///   - isImage: 是否是图片还是视频
+///   - completion: （isSuccess=成功、失败，identifier=相册标识符）
++(void)saveToAlbum:(id)data isImage:(BOOL)isImage completion:(void (^)(BOOL isSuccess,NSString * _Nullable identifier))completion{
+    __block PHObjectPlaceholder *placeHolder ;
+    UIImage *saveImage = nil;
+    NSURL *saveUrl = nil;
+    if ([data isKindOfClass:[UIImage class]]) {
+        saveImage = data;
+    }else if([data isKindOfClass:[NSString class]]){
+        saveUrl = [NSURL fileURLWithPath:data];
+    }else if([data isKindOfClass:[NSURL class]]){
+        saveUrl = data;
+    }else{
+        if (completion) {
+            completion(NO,@"data Type error");
+        }
+        return;
+    }
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+        PHAssetChangeRequest *assetChangeRequest ;
+        if (isImage) {
+            if (saveImage) {
+                assetChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromImage:saveImage];
+            }else if(saveUrl){
+                assetChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromImageAtFileURL:saveUrl];
+            }
+        }else{
+            if(saveUrl){
+                assetChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromVideoAtFileURL:saveUrl];
+            }
+        }
+        placeHolder = assetChangeRequest.placeholderForCreatedAsset;
+    } completionHandler:^(BOOL success, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSString *identifier = placeHolder.localIdentifier;
+            if (success) {
+//                NSLog(@"FanAuthManager-保存到相册成功");
+                if (completion) {
+                    completion(YES,identifier);
+                }
+            } else if (error) {
+                if (completion) {
+                    completion(NO,error.localizedDescription);
+                }
+//                NSLog(@"FanAuthManager-图片保存出错:%@",error.localizedDescription);
+            }
+        });
+    }];
 }
 #pragma mark - 相机麦克风权限
 /// 请求相机权限
